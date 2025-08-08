5b0e8e03e26ba375eedefa297f003c6334cf3a25
Your Name <kerem-1034003>
2025-08-08 16:23:26 +0200

    user story 9, Green: test passed

---
+++ src/main/java/com/booleanuk/core/Filling.java
@@ -0,0 +1,25 @@
+package com.booleanuk.core;
+
+public class Filling {
+    private String sku;
+    private String name;
+    private double price;
+
+    public Filling(String sku, String name, double price) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
