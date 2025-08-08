e9d22f721edf016dfed2d6003d5fa924fc5363bd
Your Name <kerem-1034003>
2025-08-08 14:06:02 +0200

    user story 2 Red: Test

--- src/test/java/com/booleanuk/core/BasketTest.java
+++ src/test/java/com/booleanuk/core/BasketTest.java
@@ -16,4 +16,21 @@ public class BasketTest {
         Assertions.assertEquals(1, basket.getItems().size());
         Assertions.assertEquals("Onion", basket.getItems().get(0).getVariant());
     }
+
+    @Test
+    public void testRemoveBagelFromBasket() {
+        Basket basket = new Basket();
+        Bagel bagel1 = new Bagel("BGLO", "Bagel", "Onion", 0.49);
+        Bagel bagel2 = new Bagel("BGLP", "Bagel", "Plain", 0.39);
+
+        basket.addItem(bagel1);
+        basket.addItem(bagel2);
+
+        boolean removed = basket.removeItem(bagel1);
+
+        Assertions.assertTrue(removed);
+        Assertions.assertEquals(1, basket.getItems().size());
+        Assertions.assertEquals("Plain", basket.getItems().get(0).getVariant());
+    }
+
 }
